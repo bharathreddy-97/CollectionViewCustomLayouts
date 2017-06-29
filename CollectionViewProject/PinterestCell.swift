eec9eedf142c50fa3d86b16e33b3da356d825a49
Next <next@techconsultant-01.local>
2017-06-29 13:35:28 +0530

    Re-arranaged into group

--- CollectionViewProject/PinterestCell.swift
+++ CollectionViewProject/PinterestCell.swift
@@ -14,6 +14,7 @@ class PinterestCell: UICollectionViewCell {
     @IBOutlet weak var mImageView: UIImageView!
     
     
+    @IBOutlet weak var mCommentLabel: UILabel!
     @IBOutlet weak var mCaptionLabel: UILabel!
     @IBOutlet weak var mImageViewHeightConstraint: NSLayoutConstraint!
     
